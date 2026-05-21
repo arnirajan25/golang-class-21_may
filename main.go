aa09c9566b59286f9a197880cb2fd41a6b9dff86
nirajan <nirajanaryal25@gmail.com>
2026-05-21 01:54:48 +0000

    golang class 21st may

---
+++ main.go
@@ -0,0 +1,21 @@
+
+package main
+import "fmt"
+var num int = 10
+var decimal float64 = 10.10
+var True bool = true
+var suhag string
+var mahim string = "ncmt_student"
+var safal string = "ncmt_bcs_student"
+func main(){
+suhag="python guy"
+var name string= "nirajan"
+temp :=30.85
+var temp_two float64= 32.32
+var age int= 20
+fmt.Println("temp:%.2f\n",temp)
+fmt.Println("name:%s\n",name)
+fmt.Println("age:%d\n",age)
+fmt.Println("temp:%.2f\n",temp_two)
+fmt.Println(suhag,safal,mahim,num,decimal,True)
+}
